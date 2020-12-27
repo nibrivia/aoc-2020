ed27de9221d22484d938b6c31b8787b6adf98b37
Olivia Brode-Roger <nibr@mit.edu>
2020-12-26 19:15:11 -0500

    day 5

---
+++ day-5.py
@@ -0,0 +1,12 @@
+from utils import *
+
+seats = read_day(5)
+bin_str = [s.replace("F", "0").replace("L", "0")
+        .replace("B", "1").replace("R", "1") for s in seats]
+seat_ids = [int(s, 2) for s in bin_str]
+print(max(seat_ids))
+seats = set(x for x in range(128*8))
+#seats.substract(set(seat_ids))
+print(seats-set(seat_ids))
+
+print("done")
